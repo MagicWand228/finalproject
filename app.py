07e3c4cf4473535e506af1f66565892d8b1bc57a
Maksum Grechka <mimaxyk@gmail.com>
2025-01-25 16:00:59 +0200

    replace

--- app.py
+++ app.py
@@ -10,8 +10,15 @@ socketio = SocketIO(app)
 def index():
     if request.method == 'POST':
         session['nickname'] = request.form['nickname']
-        return redirect(url_for('chat'))
+        return redirect(url_for('home'))
     return render_template('index.html')
+
+
+@app.route('/home')
+def home():
+    if 'nickname' not in session:
+        return redirect(url_for('index'))
+    return render_template('home.html', nickname=session['nickname'])
 
 
 @app.route('/chat')
@@ -21,10 +28,24 @@ def chat():
     return render_template('chat.html', nickname=session['nickname'])
 
 
+@app.route('/stories', methods=['GET', 'POST'])
+def stories():
+    if 'nickname' not in session:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        story = request.form['story']
+        if len(story) >= 24:
+            with open('stories.txt', 'a') as file:
+                file.write(f'{session["nickname"]}: {story}\n')
+    with open('stories.txt', 'r') as file:
+        stories_list = file.readlines()
+    return render_template('stories.html', nickname=session['nickname'], stories=stories_list)
+
+
 @socketio.on('message')
 def handle_message(msg):
     nickname = session.get('nickname', 'Анонім')
-    send(f'{nickname}: {msg}', broadcast=True)
+    send(f'<b style="color: blue;">{nickname}</b>: <span style="color: black;">{msg}</span>', broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
